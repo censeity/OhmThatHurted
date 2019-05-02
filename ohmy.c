250f86a53945856643068235d9d462e7cee55ce0
Andy Garcia <47613990+censeity@users.noreply.github.com>
2019-05-01 21:39:24 -0400

    Update ohmy.c

--- ohmy.c
+++ ohmy.c
@@ -4,34 +4,34 @@
 
 int main()
 {
-    int Voltage= 0, Current= 0, Resistance= 0, Result= 0, Choice= 0;
-    int v= 0, i= 0, r= 0;
+    float Voltage= 0, Current= 0, Resistance= 0, Result= 0, Choice= 0;
+    float v= 0, i= 0, r= 0;
     printf("What variable do you have missing?\n");
     printf(" 1. V Volts\n 2. I Current\n 3. Resistance\n");
-    scanf("%d", &Choice);
+    scanf("%f", &Choice);
     if(Choice == 1){
     printf("What is the value of resistance: ");
-    scanf("%d", &r);
+    scanf("%f", &r);
     printf("What is the current value: ");
-    scanf("%d", &i);
+    scanf("%f", &i);
     Result= r * i;
-    printf("The voltage is% d volts\n",Result);
+    printf("The voltage is% f volts\n",Result);
     }
     else if (Choice == 2){
     printf("What is the value of the voltage: ");
-    scanf("%d", &v);
+    scanf("%f", &v);
     printf("What is the value of resistance: ");
-    scanf("%d", &r);
+    scanf("%f", &r);
     Result= v / r;
-    printf("The current is% d amps\n",Result);
+    printf("The current is% f amps\n",Result);
     }
     else if (Choice == 3){
     printf("What is the value of the voltage: ");
-    scanf("%d", &v);
+    scanf("%f", &v);
     printf("What is the current value: ");
-    scanf("%d", &i);
+    scanf("%f", &i);
     Result= v / i;
-    printf("The resistance is% d ohms\n",Result);
+    printf("The resistance is% f ohms\n",Result);
     }
 
 
